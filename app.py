dc93da560ba8cb14d285a7a4c323cd6411091a30
csbustamantea <84584223+csbustamantea@users.noreply.github.com>
2026-05-23 23:14:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,20 @@
 def sumar(a, b):
     return a + b
 
+
 def restar(a, b):
     return a - b
 
+
 def multiplicar(a, b):
     return a * b
+
 
 def dividir(a, b):
     if b == 0:
         raise ValueError("No se puede dividir entre cero")
     return a / b
+
 
 if __name__ == "__main__":
     print("Calculadora lista")
